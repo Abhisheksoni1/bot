3f56ea20683a0a068a066e7898594997cf94f981
abhisheksoni1 <abhisheksoni500@gmail.com>
2017-11-02 02:02:02 +0530

    bot work done

--- bitoasis.py
+++ bitoasis.py
@@ -1,17 +1,22 @@
 import requests
 import json
+from urllib import urlencode
 
 
 class BITOASIS(object):
     CONTANT = 3.6727299
     BASE_URL = "https://api.bitoasis.net/v1/exchange/"
 
-    def __init__(self):
+    def __init__(self, client_id, client_secret, username, password):
         self.buy_price = 0
         self.currency_1 = "ETH"
         self.currency_2 = "AED"
         self.trading_fee = 0.5
         self.last_price = 0
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.username = username
+        self.password = password
 
     def get_buy_price(self):
         url = self.BASE_URL + "ticker/{}-{}".format(self.currency_1, self.currency_2)
@@ -20,16 +25,41 @@ class BITOASIS(object):
         self.buy_price = float(data['ask'])/self.CONTANT
         return self.buy_price
 
+    def login(self):
+        data = {
+            'grant_type': "password",
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'username': self.username,
+            'password': self.password
+                }
+        url = "https://api.bitoasis.net/v1/access_token"
+        value = urlencode(data)
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        ret = requests.post(url, data=value, headers=headers)
+        print(ret.status_code)
+        data = json.loads(ret.text)
+        print data
+
     def place_buy_order(self, **params):
-        values = "{'side':'buy','type':'limit','pair':'{}-{}','amount':{},'price': {}}".\
-            format(self.currency_1, self.currency_2, params['amount'], params['price'])
+        self.login()
+        value = json.dumps({
+            'side': 'buy',
+            'type': 'limit',
+            'pair': "-".join([self.currency_1, self.currency_2]),
+            'amount': params['amount'],
+            'price': params['price']
+                  })
         headers = {
             'Content-Type': 'application/json'
         }
         url = self.BASE_URL + "order"
-        ret = requests.post(url, values, headers).text
+        ret = requests.post(url, data=value, headers=headers).text
         print(ret)
 
 
-b = BITOASIS()
-b.place_buy_order(amount=0.1, price=6100)
+if __name__ == '__main__':
+    b = BITOASIS("client_id", "secret", "username", "password")
+    b.place_buy_order(amount=0.1, price=6100)
--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import cex_io
 import bitoasis
 import time
 import settings
+import threading
 
 if __name__ == '__main__':
-    b = bitoasis.BITOASIS()
+    b = bitoasis.BITOASIS(settings.BITOASIS_CLIENT_ID, settings.BITOASIS_CLIENT_SECRET,
+                          settings.BITOASIS_CLIENT_USERNAME, settings.BITOASIS_CLIENT_PASSWORD)
     ci = cex_io.CEX_IO(settings.CEX_IO_USERNAME, settings.CEX_IO_API_KEY, settings.CEX_IO_API_SECRET)
     while True:
+        buy_amount = settings.BITOASIS_BUY_AMOUNT
         sell_price = ci.get_sell_price()
         buy_price = b.get_buy_price()
+        eth_used = buy_amount * buy_price
+        sell_amount = sell_price/eth_used
         per_change = (float(sell_price - buy_price)/sell_price)*100
         print("Sell Price is ${} \t Buy Price is ${} \t".format(sell_price, buy_price))
         print("Percentage change in prices are {} %".format(per_change))
+        if per_change > 1:
+            # place a buy and sell order in both exchanges
+            threading.Thread(target=b.place_buy_order, kwargs=({'amount': buy_amount, 'price': buy_price})).start()
+            threading.Thread(target=ci.place_sell_order, kwargs=({'amount': sell_amount, 'price': sell_price})).start()
         time.sleep(1)
